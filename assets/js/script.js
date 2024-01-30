77b8b320b130f1204d5f69a8118be9d263fc9230
reyad-hasan <138390518+reyad-hasan@users.noreply.github.com>
2024-01-30 15:06:12 +0600

    i have sloved parallax effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,35 +109,17 @@ addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], 'mouseover', autoSlid
 window.addEventListener('load', autoSlide);
 
 // parallax effect
-
-// const parallaxItems = document.querySelectorAll('[data-parallax-item]');
-
-// let x, y;
-
-// window.addEventListener('mousemove', function (event) {
-//     x = (event.clientX / window.innerWidth * 10) - 5;
-//     y = (event.clientY / window.innerWidth * 10) - 5;
-
-//     // reverse the number eg. 20 --> -20, -5 --> 5
-
-//     x = x - (x * 2);
-//     y = y - (y * 2);
-
-//     for (let i = 0; len = parallaxItems.length; i < len) {
-//         x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
-//         y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
-//         parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
-//     }
-
-// });
-
-document.addEventListener('mousemove', parallax);
-function parallax (e) {
-    document.querySelectorAll('.data-parallax-item').forEach(function(move){
-        var moving_value = move.getAttribute('data-value');
-        var x = e.clientX * moving_value;
-        var y = e.clientY * moving_value;
-
-        move.style.transform = 'translateX(' + x + 'px) translateY(' + y +'px)';
-    });
-}
+const parallaxItems = document.querySelectorAll('[data-parallax-item]');
+let x, y;
+window.addEventListener('mousemove', function (event) {
+    x = (event.clientX / window.innerWidth * 10) - 5;
+    y = (event.clientY / window.innerWidth * 10) - 5;
+    // reverse the number eg. 20 --> -20, -5 --> 5
+    x = x - (x * 2);
+    y = y - (y * 2);
+    for (let i = 0, len = parallaxItems.length; i < len; i++) {
+        x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
+        y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
+        parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+    }
+});
